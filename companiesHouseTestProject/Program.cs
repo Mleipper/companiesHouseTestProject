9b2d2ffeab71b67005ba5de44516fedc5f4cd0b6
Michael Leipper <michael.leipper@Gmail.com>
2020-09-01 14:19:14 +0100

    Add project files.

---
+++ companiesHouseTestProject/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace companiesHouseTestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
