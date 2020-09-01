20f18f9591c0b832d42d659bd12930b82c9acc27
Michael Leipper <michael.leipper@Gmail.com>
2020-09-01 15:01:33 +0100

    Add Concreate classes that imlement interfaces.

---
+++ companiesHouseTestProject/CompaniesHouseAPIClient/CompaniesHouseAPIClient.cs
@@ -0,0 +1,14 @@
+﻿using companiesHouseTestProject.CompaniesHouseAPIClient.CompaniesHouseResponses;
+using System;
+using System.Threading.Tasks;
+
+namespace companiesHouseTestProject.CompaniesHouseAPIClient
+{
+    class CompaniesHouseAPIClient : ICompaniesHouseApiClient
+    {
+        public Task<CompaniesHouseSearchResponse> PagedCompaniesSearch(string searchString, int? pageNumber, int? itemsPerPage)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ companiesHouseTestProject/EDocumentsTestClient/EDocumentsTestClient.cs
@@ -0,0 +1,24 @@
+﻿using companiesHouseTestProject.EDocumentsTestClient.EDocumentsModels;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace companiesHouseTestProject.EDocumentsTestClient
+{
+    public class EDocumentsTestClient : IEdocumentsTestClient
+    {
+        public Task<List<string>> GetChanges(CompanyModel company_model)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<CompanyModel> GetGivenCompany(int company_number)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Task<List<CompanyModel>> Search(string Company_name)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
